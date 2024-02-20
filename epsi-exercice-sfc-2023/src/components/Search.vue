530a2112699dc3380899eecb3348b14da4d6d5e8
Guillaumas <guillaume.gillet.pro@gmail.com>
2024-02-20 11:43:49 +0100

    search + search page base

--- epsi-exercice-sfc-2023/src/components/Search.vue
+++ epsi-exercice-sfc-2023/src/components/Search.vue
@@ -1,11 +1,19 @@
 <script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+let searchQuery = ref('');
+
+const searchAlbums = async () => {
+  const response = await axios.get(`https://api.deezer.com/search/album?q=${searchQuery.value}`);
+  emit('search-completed', response.data.data);
+};
 
 </script>
 
 <template>
-
+  <form @submit.prevent="searchAlbums">
+    <input v-model="searchQuery" type="text" placeholder="Rechercher un album">
+    <button type="submit">Rechercher</button>
+  </form>
 </template>
-
-<style scoped>
-
-</style>
--- epsi-exercice-sfc-2023/src/views/SearchPage.vue
+++ epsi-exercice-sfc-2023/src/views/SearchPage.vue
@@ -1,9 +1,18 @@
 <script setup>
+import { ref } from 'vue';
 import Search from "@/components/Search.vue";
+import MusicCard from "@/components/MusicCard.vue";
+let searchResults = ref([]);
+
+const handleSearchCompleted = (results) => {
+  searchResults.value = results;
+};
+
 </script>
 
 <template>
   <main>
-    <Search />
+    <Search @search-completed="handleSearchCompleted" />
+    <MusicCard v-for="album in searchResults" :key="album.id" :album="album" />
   </main>
 </template>
